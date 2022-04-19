1329bad7f6eec8b7436339a91abb0edfa93321a2
Paula <72952547+pshin15@users.noreply.github.com>
2022-04-19 13:20:35 -0400

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -1,2 +1,16 @@
 class Game:
+  """Steps for playing the game
+  """
   def __init__(self, turns, names):
+  def Turn()
+  def Attack()
+  def Surrender()
+  def Health()
+  def NextBattle()
+
+class Monster:
+  def __init__(self, name, power):
+  def Player_monster():
+  def Computer_monster():
+  def Player_Skill_list():
+  def Monster_skill_list():
