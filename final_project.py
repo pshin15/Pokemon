39fd095284cca1a0b8450358ba15e331e73d608c
Paula <72952547+pshin15@users.noreply.github.com>
2022-04-19 14:49:41 -0400

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -1,16 +1,40 @@
+"""A turn based fantasy game that allows users to pick their character and power to fight against main monster"""
+
 class Game:
-  """Steps for playing the game
-  """
-  def __init__(self, turns, names):
-  def Turn()
-  def Attack()
-  def Surrender()
-  def Health()
-  def NextBattle()
+    """Steps for playing the game
+    
+    Attributes:
+        turns (int):
+        names (str): 
+    """
+    def __init__(self, turns, names):
+        """Initializes Game object.
+        """
+    def Turn():
+    def Attack():
+        """Specifies method of attack.
+        """
+    def Surrender():
+    def Health():
+        """Displays a health/XP bar that changes based on attacks
+        """
+    def NextBattle():
+
 
 class Monster:
-  def __init__(self, name, power):
-  def Player_monster():
-  def Computer_monster():
-  def Player_Skill_list():
-  def Monster_skill_list():
+    """Class for the monster the user will fight against.
+    
+    Attributes:
+        name (str): 
+        power (str): 
+    """
+    def __init__(self, name, power):
+    def Player_monster():
+    def Computer_monster():
+    def Player_Skill_list():
+    def Monster_skill_list():
+
+
+if __name__ == "__main__":
+    args = parse_args(sys.argv[1:])
+    main(args.file)
