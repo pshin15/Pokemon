779a2f7a844f74ae15adf0c9bf1eba4baa8627b6
Kamari Trotz <ktrotz@umd.edu>
2022-05-16 19:23:19 -0400

    added to docstrings

--- final_project.py
+++ final_project.py
@@ -1,21 +1,52 @@
+from argparse import ArgumentParser
+import random
+import sys
+
+
+
 """A turn based fantasy game that allows users to pick their character and power to fight against main monster"""
 
 class Game:
     """Steps for playing the game
     
     Attributes:
-        turns (int):
-        names (str): 
+        names (list): 
     """
-    def __init__(self, turns, names):
+    def __init__(self):
         """Initializes Game object.
+        
+        Side effects:
+            Sets the names attributes
         """
-    def Turn():
-        """Allows player to take turns
-        """
-    def Attack():
+        self.names = ['Pikachu','Scizor','Snorlax','Ninetales','Squirtle','Charizard']
+        self.health = 100
+        self.hp_val = 20
+              
+    def Attack(self):
         """Specifies method of attack.
         """
+        
+        attack_method = int(input('choose attack method (attack - 1, special attack - 2)'))
+        hp_subtract_special= random.randint(2,6)
+        hp_subtract_normal = random.randint(1,3)
+        special_attack_damage = random.randint(8,30)
+        normal_attack_damage = random.randint(2,15)
+        if attack_method == 1:
+            self.hp_val-= hp_subtract_normal
+            self.health -= normal_attack_damage
+        elif attack_method == 2:
+            self.hp_val -= hp_subtract_special
+            self.health -= special_attack_damage
+        
+    def Turn(self,opponent):
+            """Allows player to take turns
+            """
+            player1 = input('Choose your character!(Pikachu,Scizor,Snorlax,Ninetales,Squirtle,Charizard)')
+            opponent = random.choice(self.names)
+            count = 0
+            
+         
+        
     def Surrender():
         """Allows player to surrender
         """
@@ -36,6 +67,9 @@ class Monster:
     """
     def __init__(self, name, power):
         """Initializes Monster object
+        
+        Side effects:
+            Sets the name and power attributes
         """
     def Player_monster():
         """ Displays player's monsters
